13fc3da2ba43cb15a4af26e3826d6890b8742d87
Stephen Vizzusi <svizzusi13@gmail.com>
2023-09-08 12:20:00 -0700

    modified the index.js to make sure that mongoose.connect is finished before allowing my app to serve requests

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-// Install and import packages
-const express = require('express') //Require express module
-const app = express() //Create the instance of express
-const mongoose = require('mongoose') //Require the mongoose module
-const cors = require('cors') //Cross Origin Resource Sharing - allows frontend to talk to the server
-const todoModel = require('./model/todoSchema.js') //Require the schema data structure
-const userModel = require('./model/userSchema.js') //Require the schema data structure
-require('dotenv').config({path: './config/.env'}) //require the environment variable
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const todoModel = require('./model/todoSchema.js');
+const userModel = require('./model/userSchema.js');
+require('dotenv').config({ path: './config/.env' });
 
-const userRoutes = require('./routes/users.js')
-const todoRoutes = require('./routes/todos.js')
+const userRoutes = require('./routes/users.js');
+const todoRoutes = require('./routes/todos.js');
 
-app.use(cors()) //Use cors middleware in the express app
-app.use(express.json()) // Middleware: Parse incoming JSON data
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use('/users', userRoutes)
-app.use('/todos', todoRoutes)
+app.use(cors());
+app.use(express.json());
 
-// Establish a connection to the database and start the server
-mongoose.connect(process.env.DB_STRING)
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log(`Connected to DB & listening on port ${process.env.PORT}`)
-        })
-    })
-    .catch((err) => {
-        console.log(err) // Log any database connection errors
-    })
+// Define a function to connect to the database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_STRING);
+    console.log('Connected to MongoDB');
+    // Start the server after the database connection is established
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+};
+
+// Call the connectDB function to initiate the database connection
+connectDB();
+
+// Use your routes here
+app.use('/users', userRoutes);
+app.use('/todos', todoRoutes);
+
+
+
+
+
+
